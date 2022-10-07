213e9a85d89f0221dc4e4449f64378d1e6331a2f
Christina Liang <christina.liang@outlook.com>
2022-10-07 16:46:44 -0400

    initial detection

---
+++ src/utilities.js
@@ -0,0 +1,18 @@
+export const drawRect = (detections, ctx) => {
+  detections.forEach((prediction) => {
+    const [x, y, width, height] = prediction["bbox"];
+    const text = prediction["class"];
+
+    // styles
+    const color = "green";
+    ctx.strokeStyle = color;
+    ctx.font = "18px Arial";
+    ctx.fillStyle = color;
+
+    // draw rectangles and text
+    ctx.beginPath();
+    ctx.fillText(text, x, y);
+    ctx.rect(x, y, width, height);
+    ctx.stroke();
+  });
+};
